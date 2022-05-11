cd0f2a94dfc6e52a5cdf168dc14bb9499c4310a2
Taite Jernigan <taite5100@gmail.com>
2022-05-11 14:43:56 -0700

    Added tests

---
+++ test.js
@@ -0,0 +1,33 @@
+const app = require('./server/index.js');
+const supertest = require('supertest');
+const allCities = require('./cities.js')
+
+function countCities(citiesObj) {
+  let count = 0;
+  for (let cityType in citiesObj) {
+    count += cityType.length
+  }
+  return count
+}
+const numberOfCities = countCities(allCities)
+
+const mockBeachCityData = {}
+
+test('GET /cities will retrieve all cities', async () => {
+  await supertest(app).get('/cities')
+  .send()
+  .expect(200)
+  .then((res) => {
+    expect(res.body.length).toBe(numberOfCities)
+  });
+});
+
+
+test('PUT /cities will update said city type', async () => {
+  await supertest(app).put('/cities?type=beachCities')
+  .expect(200)
+  .then((res) => {
+    expect(res.body.command).toBe('INSERT')
+    expect(res.body.rowCount).toBe(allCities.beachCities.length + 2)
+  });
+});
